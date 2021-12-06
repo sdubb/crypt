2d29d1128a8d87e72ab86a5cec92a878d2252344
SHUBHAM DUBEY <shuubhamdubey3131@gmail.com>
2021-12-06 17:50:29 +0000

    Updates

---
+++ precache-manifest.e7353cb4fda011bbab13412a99884881.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388554b3ff9e6ee25a33ea0a78989b50",
+    "url": "/crypt/index.html"
+  },
+  {
+    "revision": "cb4eba0f78c665f8f2b9",
+    "url": "/crypt/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "37e7bc9492a9c67e9b0b",
+    "url": "/crypt/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "cb4eba0f78c665f8f2b9",
+    "url": "/crypt/static/js/2.3f960e1a.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/crypt/static/js/2.3f960e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e7bc9492a9c67e9b0b",
+    "url": "/crypt/static/js/main.f83a8ac0.chunk.js"
+  },
+  {
+    "revision": "acee2e8c7ae8d94e59a7",
+    "url": "/crypt/static/js/runtime-main.98f39163.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/crypt/static/media/photo.d6cbd8e6.png"
+  }
+]);
