8d45904fa3045f1d74794526613b09ff8efeb51b
SHUBHAM DUBEY <shuubhamdubey3131@gmail.com>
2021-12-21 07:41:51 +0000

    Updates

---
+++ precache-manifest.31f24bf0314e40f90125c36d05d87220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6b084ac4ea2c11bd2e9106267029a8",
+    "url": "/meta/index.html"
+  },
+  {
+    "revision": "02b252bc419cb6788445",
+    "url": "/meta/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "4e2a839a048f6b6c5d26",
+    "url": "/meta/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "02b252bc419cb6788445",
+    "url": "/meta/static/js/2.9420fea8.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/meta/static/js/2.9420fea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2a839a048f6b6c5d26",
+    "url": "/meta/static/js/main.38bbf6cf.chunk.js"
+  },
+  {
+    "revision": "026e278ba0803152d32b",
+    "url": "/meta/static/js/runtime-main.10559de6.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/meta/static/media/photo.d6cbd8e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crypt/precache-manifest.e7353cb4fda011bbab13412a99884881.js"
+  "/meta/precache-manifest.31f24bf0314e40f90125c36d05d87220.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crypt/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
